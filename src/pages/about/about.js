7e7f0f7770f45d9edb527db0e8431d23c10cbde5
Dan Card <dcard@umich.edu>
2023-12-01 17:20:06 -0600

    Added basic routing and navbar

---
+++ src/pages/about/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold underline">
+        This is the about page!
+      </h1>
+      <p className='text-blue-500 text-3xl font-bold underline'>
+        testing
+      </p>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/pages/home/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p className='text-blue-500 text-3xl font-bold underline'>
+        testing
+      </p>
+    </section>
+  );
+}
+
+export default Home;
