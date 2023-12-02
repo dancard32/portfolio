e3af0e8f86cd6a9baf90cc6cc26b2f0d936e19c2
Dan Card <dcard@umich.edu>
2023-12-01 20:10:15 -0600

    Dark mode toggle enabled

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import About from "./pages/about/about";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import NavBar from './components/navbar'
+import { DarkThemeToggle, Flowbite } from 'flowbite-react';
 
 function App() {
   return (
+    <Flowbite>
     <BrowserRouter>
-      <div className="App">
+      <div className="App bg-primary-600" >
         <NavBar />
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
+      <DarkThemeToggle />
     </BrowserRouter>
+    </Flowbite>
   );
 }
 
 export default App;
-
-
-
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,10 @@
 import { Navbar } from 'flowbite-react';
 import logo from '../assets/images/logo.png';
+import { DarkThemeToggle, Flowbite } from 'flowbite-react';
 
 function Component() {
   return (
+    <Flowbite>
     <Navbar fluid rounded>
       <Navbar.Brand href="https://flowbite-react.com">
         <img src={logo} className="mr-3 h-6 sm:h-9" alt="NavBar Logo" />
@@ -11,8 +13,10 @@ function Component() {
       <Navbar.Collapse className='flex items-center text-2xl'>
         <Navbar.Link href="/home" active>Home</Navbar.Link>
         <Navbar.Link href="/about">About</Navbar.Link>
+        <DarkThemeToggle/>        
       </Navbar.Collapse>
     </Navbar>
+    </Flowbite>
   );
 }
 
